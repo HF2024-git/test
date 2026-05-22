87392ff8d5919a5f02f0969d75cc192c3dbeb8bd
HF2024-git <how-free@163.com>
2026-05-22 22:39:54 +0800

    Remove slider and multiselect from test.py
    Removed number slider and multiselect options from the UI.

--- test.py
+++ test.py
@@ -9,10 +9,6 @@ def main():
         st.write("请输入你的名字")
     if st.button("点击我"):
         st.write("按钮被点击了！")
-    add_number = st.slider('选择一个数字', 0, 100)
-    st.write(f"你选择的数字是: {add_number}")
-    options = st.multiselect('选择一些选项', ['苹果', '香蕉', '樱桃'])
-    st.write('你选择的水果是:', options)
 
 if __name__ == "__main__":
     main()
